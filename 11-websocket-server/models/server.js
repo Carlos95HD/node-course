66adfb2d026a30fa4f83dcaebc9cd783d1f397ec
Carlos <hernandeasi7@gmail.com>
2022-08-24 01:37:01 -0300

    Sockets | Bases

---
+++ 11-websocket-server/models/server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors');
+const { socketController } = require('../sockets/contollers');
+
+class Server{
+  constructor(){
+    this.app = express();
+    this.port = process.env.PORT
+    this.server = require('http').createServer(this.app);
+    this.io = require('socket.io')(this.server);
+
+    this.paths = {}
+
+    //Middlewares
+    this.middlewares();
+
+    //Rutas de mi aplicación
+    this.routes();
+
+    //Sockets
+    this.sockets();
+  }
+
+  middlewares(){
+    //CORS
+    this.app.use(cors());
+
+    //Directorio Publico
+    this.app.use(express.static( 'public'));
+  }
+
+  routes(){
+    // this.app.use( this.paths.auth, require('../routes/auth'));
+  }
+
+  sockets(){
+    this.io.on('connection', socketController);
+  }
+
+  listen(){
+    this.server.listen( this.port,() => {
+      console.log('Server listening on port ' + this.port);
+    })
+  }
+}
+
+module.exports = Server;
